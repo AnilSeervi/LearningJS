aa7a44bc7e1188e1d56d09761a827bc01c6bf95d
Anil Seervi <anil13112000@gmail.com>
2020-05-28 16:07:59 +0530

    Usage of Conditionals in JS

---
+++ Conditionals.js
@@ -0,0 +1,38 @@
+// A simple if statement
+var message = 'helo';
+if (message === 'hello') {
+	console.log(message);
+}
+//A simple if..else statement
+if (message) {
+	console.log(message);
+} else {
+	console.log('There was no message!');
+}
+// A if..else_if..else ladder
+if (message === 'hello') {
+	console.log(message + ' ' + 'World');
+} else if (message !== 'hello') {
+	console.log(`The message was not hello, but: ${message}`);
+} else {
+	console.log('There was no message!');
+}
+// Use of AND(&&) Logical Operator
+if (message === 'hello' && message) {
+	console.log(message);
+}
+//Use of OR(||) Logical Operator
+if (message === 'hello' || message) {
+	console.log(message);
+}
+//Usage of switch case statement
+switch (message) {
+	case 'hello':
+		console.log('Your message was Hello!');
+		break;
+	default:
+		console.log(`Your message was ${message}`);
+}
+//Usage of ternary Operator. Syntax: (condition) ? run this code : run this code instead
+var greetings = message ? `Your Greetings was ${message}` : `Your Greetings was ${message}`;
+console.log(greetings);

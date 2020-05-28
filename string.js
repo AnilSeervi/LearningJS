329fb8213809ed5992cc6570c2205c84b94d90ad
Anil Seervi <anil13112000@gmail.com>
2020-05-28 14:37:10 +0530

    Contains some String methods

---
+++ string.js
@@ -0,0 +1,13 @@
+// Contains indexOf, slice, toUpperCase and toLowerCase, and Replace.
+var name = 'Google';
+console.log(name[name.length - 1]);
+console.log(name.indexOf('e'));
+console.log(name.indexOf('s'));
+
+console.log(name.slice(0, 3));
+console.log(name.slice(2));
+
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+
+console.log(name.replace('gle', 'girl'));

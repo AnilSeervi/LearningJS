a332cad961bae18101aba504bedbb82c6c7ff961
Anil Seervi <anil13112000@gmail.com>
2020-05-30 15:42:52 +0530

    Added Constructor and Factory Funtions to create Objects.

--- objects.js
+++ objects.js
@@ -12,11 +12,27 @@ car.name = "honda";
 car.year = 2019;
 console.log(car);
 
+// Constructor Function to create objects. The name of the object/function should be in Pascal Case: OneTwoThree
 function Animal(name, breed, age) {
   this.Name = name;
   this.Breed = breed;
   this.Age = age;
 }
-
+// After creating the Constructor function. We need to instantiate the object to access it, using 'new' keyword
 const dog = new Animal("Dog", "Dalmatian", 10);
 console.log(dog);
+
+// Factory Function to create objects. The name of the object/function should be in Camel Case: oneTwoThree
+function createCircle(radius) {
+  return {
+    radius, // Just the key in the object denotes that key : key(i.e., radius : radius)
+    diameter: radius * 2,
+    draw() {
+      console.log("Drawing a Circle");
+    },
+  };
+}
+
+const myCircle = createCircle(2);
+console.log(myCircle);
+myCircle.draw();
